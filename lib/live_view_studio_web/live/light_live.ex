899a8df0a1b8fae922861936c188dbed9ef579a9
Fadhli Rahim <fadhlirahim@gmail.com>
2022-08-18 10:48:47 +0800

    Added exercise refactor light live input radio boxes

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,16 +2,17 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :brightness, 10)
+    socket = assign(socket, brightness: 10, temp: 3000)
+
     {:ok, socket}
   end
 
   def render(assigns) do
-    ~L"""
+    ~H"""
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style="width: <%= @brightness %>%">
+        <span style={"background-color: #{temp_color(@temp)};width: #{@brightness}%"}">
           <%= @brightness %>%
         </span>
       </div>
@@ -31,6 +32,12 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="on">
         <img src="images/light-on.svg">
       </button>
+
+      <form phx-change="change-temp">
+        <%= for temp <- ["3000", "4000", "5000"] do %>
+          <%= change_temp_view(temp: temp, checked: temp == @temp) %>
+        <% end %>
+      </form>
     </div>
     """
   end
@@ -41,17 +48,37 @@ defmodule LiveViewStudioWeb.LightLive do
   end
 
   def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10))
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
     {:noreply, socket}
   end
 
   def handle_event("down", _, socket) do
-    socket = update(socket, :brightness, &(&1 - 10))
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
     {:noreply, socket}
   end
 
   def handle_event("off", _, socket) do
     socket = assign(socket, :brightness, 0)
     {:noreply, socket}
+  end
+
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    temp = String.to_integer(temp)
+    socket = assign(socket, temp: temp)
+    {:noreply, socket}
+  end
+
+  defp temp_color(3000), do: "#F1C40D"
+  defp temp_color(4000), do: "#FEFF66"
+  defp temp_color(5000), do: "#99CCFF"
+
+  defp change_temp_view(assigns) do
+    assigns = Enum.into(assigns, %{})
+
+    ~L"""
+      <input type="radio" id="<%= @temp %>"" name="temp" value="<%= @temp %>"
+          <%= if @checked, do: "checked" %> />
+      <label for="<%= @temp %>"><%= @temp %></label>
+    """
   end
 end
