ef6ca519f3ef01d6e66143953f8f83c584b0245c
Fadhli Rahim <fadhlirahim@gmail.com>
2022-08-20 18:12:32 +0800

    PubSub for server live

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -5,6 +5,8 @@ defmodule LiveViewStudioWeb.ServersLive do
   alias LiveViewStudio.Servers
 
   def mount(_params, _session, socket) do
+    if connected?(socket), do: Servers.subscribe()
+
     servers = Servers.list_servers()
 
     changeset = Servers.change_server(%Server{})
@@ -18,42 +20,6 @@ defmodule LiveViewStudioWeb.ServersLive do
 
     {:ok, socket}
   end
-
-  # def handle_params(_params, _url, socket) do
-  #   if socket.assigns.live_action == :new do
-
-  #     # The live_action is "new", so the form is being
-  #     # displayed. Therefore, assign an empty changeset
-  #     # for the form. Also don't show the selected
-  #     # server in the sidebar which would be confusing.
-
-  #     changeset = Servers.change_server(%Server{})
-
-  #     socket =
-  #       assign(socket,
-  #         selected_server: nil,
-  #         changeset: changeset
-  #       )
-
-  #     {:noreply, socket}
-  #   else
-
-  #     # The live_action is NOT "new", so the form
-  #     # is NOT being displayed. Therefore, don't assign
-  #     # an empty changeset. Instead, just select the
-  #     # first server in list. This previously happened
-  #     # in "mount", but since "handle_params" is always
-  #     # invoked after "mount", we decided to select the
-  #     # default server here instead of in "mount".
-
-  #     socket =
-  #       assign(socket,
-  #         selected_server: hd(socket.assigns.servers)
-  #       )
-
-  #     {:noreply, socket}
-  #   end
-  # end
 
   def handle_params(%{"id" => id}, _url, socket) do
     id = String.to_integer(id)
@@ -69,23 +35,47 @@ defmodule LiveViewStudioWeb.ServersLive do
     {:noreply, socket}
   end
 
-  def handle_params(_, _url, socket) do
-    {:noreply, socket}
+  def handle_params(_params, _url, socket) do
+    if socket.assigns.live_action == :new do
+
+      # The live_action is "new", so the form is being
+      # displayed. Therefore, assign an empty changeset
+      # for the form. Also don't show the selected
+      # server in the sidebar which would be confusing.
+      changeset = Servers.change_server(%Server{})
+
+      socket =
+        assign(socket,
+          selected_server: nil,
+          changeset: changeset
+        )
+
+      {:noreply, socket}
+    else
+
+      # The live_action is NOT "new", so the form
+      # is NOT being displayed. Therefore, don't assign
+      # an empty changeset. Instead, just select the
+      # first server in list. This previously happened
+      # in "mount", but since "handle_params" is always
+      # invoked after "mount", we decided to select the
+      # default server here instead of in "mount".
+
+      socket =
+        assign(socket,
+          selected_server: hd(socket.assigns.servers)
+        )
+
+      {:noreply, socket}
+    end
   end
 
+  # The handler for the "save" event no longer needs to
+  # prepend the new server to the list since that now
+  # happens when handling a broadcast message.
   def handle_event("save", %{"server" => params}, socket) do
     case Servers.create_server(params) do
       {:ok, server} ->
-        socket =
-          update(
-            socket,
-            :servers,
-            fn servers -> [server | servers] end
-          )
-
-        # changeset = Servers.change_server(%Server{})
-        # socket = assign(socket, changeset: changeset)
-
         # Navigate to the new server's detail page.
         # Invokes handle_params which already gets the
         # server and sets it as the selected server.
@@ -119,6 +109,9 @@ defmodule LiveViewStudioWeb.ServersLive do
     {:noreply, socket}
   end
 
+  # The handler for the "toggle-status" event no longer needs to
+  # update the list of servers since that now happens when handling
+  # a broadcast message
   def handle_event("toggle-status", %{"id" => id}, socket) do
     server = Servers.get_server!(id)
 
@@ -128,16 +121,45 @@ defmodule LiveViewStudioWeb.ServersLive do
     {:ok, _server} =
       Servers.update_server(server, %{status: new_status})
 
-    socket = assign(socket, selected_server: server)
+    {:noreply, socket}
+  end
 
-    # Refetch the list of servers to update the server's red or
-    # green status indicator displayed in the sidebar:
+  # New function to handle a broadcast message indicating
+  # that a server has been created.
+  def handle_info({:server_created, server}, socket) do
+    socket =
+      update(
+        socket,
+        :servers,
+        fn servers -> [server | servers] end
+      )
+
+    {:noreply, socket}
+  end
+
+  # New function to handle a broadcast message indicating
+  # that a server has been updated.
+  def handle_info({:server_updated, server}, socket) do
+
+    # If the updated server is the selected server,
+    # assign it so the button is re-rendered with
+    # the correct status text.
+    socket =
+      if server.id == socket.assigns.selected_server.id do
+        assign(socket, selected_server: server)
+      else
+        socket
+      end
+
+    # Refetch the list of servers so the status indicators
+    # in the sidebar are updated:
+
     servers = Servers.list_servers()
     socket = assign(socket, servers: servers)
 
-    # Or, to avoid another database hit, you can find the matching
-    # server in the current list of servers, change it, and update
-    # the list of servers:
+    # Or find the matching server in the current list of
+    # servers, change it, and update the list of servers:
+
     socket =
       update(socket, :servers, fn servers ->
         for s <- servers do
