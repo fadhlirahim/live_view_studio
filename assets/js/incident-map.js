52294be1bbcc50d76f93611fc7e8972f25ae4974
Fadhli Rahim <fadhlirahim@gmail.com>
2022-08-29 18:24:47 +0800

    use openstreetmap instead of mapbox

--- assets/js/incident-map.js
+++ assets/js/incident-map.js
@@ -4,20 +4,27 @@ class IncidentMap {
   constructor(element, center, markerClickedCallback) {
     this.map = L.map(element).setView(center, 13);
 
-    const accessToken = "your-access-token-goes-here";
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      maxZoom: 18,
+      tileSize: 512,
+      zoomOffset: -1
+    }).addTo(this.map);
 
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: accessToken,
-      }
-    ).addTo(this.map);
+    // const accessToken = "your-access-token-goes-here";
+
+    // L.tileLayer(
+    //   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    //   {
+    //     attribution:
+    //       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    //     maxZoom: 18,
+    //     id: "mapbox/streets-v11",
+    //     tileSize: 512,
+    //     zoomOffset: -1,
+    //     accessToken: accessToken,
+    //   }
+    // ).addTo(this.map);
 
     this.markerClickedCallback = markerClickedCallback;
   }
